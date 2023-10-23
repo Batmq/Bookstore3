5b9865e4e0267489adca900eca8d3390a46c9df8
SaraKathrine <72217794+SaraKathrine@users.noreply.github.com>
2023-10-23 15:34:24 +0200

    Database II

---
+++ API.php
@@ -0,0 +1,35 @@
+<?php
+require "settings/init.php";
+
+/*Henter det data der bliver sendt*/
+$data = json_decode(file_get_contents("php://input"), true);
+
+/*Hvad vil vi gerne have med i vores API?:
+password: Skal udfyldes
+nameSearch: Valgfri
+*/
+
+header("Content-Type: application/json; charset=utf-8"); /*Fortæller det at den skal opfører sig som en json fil i stedet for en PHP fil*/
+
+if(isset($data["password"]) && $data["password"] == "Bookstore"){ /*Tjekker om password er der og om det er rigtigt*/
+
+    $sql = "SELECT * FROM prdukter WHERE 1=1";
+    $bind [":bogNavn"] = $data["nameSearch"];
+
+    if(!empty($data["nameSearch"])) {
+        $sql = $sql . " AND bogNavn = :bogNavn ";
+    }
+
+    $produkter = $db -> sql($sql, $bind);
+    header("HTTP/1.1 200 OK");
+
+    echo json_encode($produkter);
+
+
+} else { /*Det er det der sker, hvis ens password er forkert*/
+    header("HTTP/1.1 401 Unauthorized"); /*Viser der er sket en fejl*/
+    $error["errorMessage"] = "Dit kodeord var forkert"; /*Fejlmeddelse*/
+
+    echo json_encode($error); /*Printer vores fejlmeddelse til skærmen i json format*/
+}
+?>
---
+++ Detail.php
@@ -0,0 +1,105 @@
+<?php
+require "settings/init.php";
+?>
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Sigende titel</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.typekit.net/ozj7xgt.css">
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body class="bgInsert">
+
+<div class="container mt-5">
+    <?php
+    $produkter = $db->sql("SELECT * FROM produkter WHERE bogID = 102" );
+    foreach($produkter as $produkt) {
+
+        ?>
+
+        <div class="row p-5 border border-5 rounded-3 bgBogVisning">
+            <div class="col-4">
+               <img class="img-fluid" src="uploads/DenBrændendeBro.JPG">
+            </div>
+            <div class="col-6 ps-3">
+             <div class="row">
+                 <div class="col-12">
+                     <h1><?php echo $produkt->bogNavn; ?></h1>
+                 </div>
+                 <div class="col-12 fs-4">
+                     <p> af <?php echo $produkt->bogForfatter; ?> </p>
+                 </div>
+                 <div class="col-12">
+                     <?php echo $produkt->bogBeskrivelse; ?>
+                 </div>
+             </div>
+                <div class="row mt-4">
+                    <div class="col-6">
+                        <p>Forlag:</p>
+                    </div>
+                    <div class="col-6">
+                        <?php echo $produkt->bogForlag; ?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <p>Udgivelsedato:</p>
+                    </div>
+                    <div class="col-6">
+                        <?php echo $produkt->bogDato; ?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <p>Kategori:</p>
+                    </div>
+                    <div class="col-6">
+                        <?php echo $produkt->bogKategori; ?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <p>Type:</p>
+                    </div>
+                    <div class="col-6">
+                        <?php echo $produkt->bogType; ?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <p>ISBN:</p>
+                    </div>
+                    <div class="col-6">
+                        <?php echo $produkt->bogISBN; ?>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-12 fw-bolder">
+                        <p><?php echo $produkt->bogPris; ?> kr. </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+        <?php
+    }
+    ?>
+</div>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
